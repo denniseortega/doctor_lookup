7af6af2b6251f9bd14a31eba150dc98f1ee541af
Dennise Ortega <dennise.i.ortega@gmail.com>
2018-06-01 14:40:36 -0700

    define variables to retrieve data info

--- src/doctor.js
+++ src/doctor.js
@@ -4,7 +4,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import './main.js';
 
-
 $(document).ready(function() {
   $('#search').submit(function(event) {
     event.preventDefault();
--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import { doctor } from './doctor.js';
+// import { doctor } from './doctor.js';
+const apiKey = process.env.exports.apiKey;
 
 $(document).ready(function() {
   $('#searchDoctor').submit(function(event) {
@@ -13,7 +14,7 @@ $(document).ready(function() {
     $('#medicalIssue').val("");
 
     let request = new XMLHttpRequest();
-    let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=a500deefc1fd833abe1de4b374a51e23`;
+    let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${doctorName}&query=${medicalIssue}&location=or-portland&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=a500deefc1fd833abe1de4b374a51e23`;
      request.onreadystatechange = function() {
        if (this.readyState === 4 && this.status === 200) {
          let response = JSON.parse(this.responseText);
@@ -25,10 +26,15 @@ $(document).ready(function() {
     request.send();
 
     let getElements = function(response) {
-      $('.showOutput').append('<p>' + `Doctor List: ${response.data[0].name}.` + '</p>');
-      $('.showOutput').append('<p>' + `The serial number is ${response.data[0].visit_address}.` + '</p>');
-      $('.showOutput').append('<p>' + `The serial number is ${response.data[0].accepts_new_patients}.` + '</p>');
-      }
+      $('.showOutput').empty();
+      response.data.forEach(function() {
+      $('.showOutput').append('<p>' + `Doctor Name: ${response.data[0].profile.first_name} ${response.data[0].profile.middle_name} ${response.data[0].profile.last_name}.` + '</p>');
+      $('.showOutput').append('<p>' + `The address is <br><div id="address">${response.data[0].practices[0].visit_address.street}<br> ${response.data[0].practices[0].visit_address.city}<br> ${response.data[0].practices[0].visit_address.state}${response.data[0].practices[0].visit_address.zip}.` + '</p>');
+      $('.showOutput').append('<p>' + `Is the doctor accepting new patients: ${response.data[0].practices[0].accepts_new_patients}.` + '</p>');
+    })
+    $("#ouput").show();
 
-    });
+
+    }
  });
+});
